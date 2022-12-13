c4dfe059108b0989f177062e25564ed56b2cdc83
Byeon_MJ <byunmj24@gmail.com>
2022-12-13 12:45:22 +0900

    idx edit 2

--- lucky/app.py
+++ lucky/app.py
@@ -18,7 +18,7 @@ for idx, col in enumerate(columns):
     # col.text_input(f'조 추첨 대상{idx+1}', key = idx)
     for idx2 in range(4):
         # key가 겹치면 안 됨
-        col.text_input(f'조 추첨 대상{idx+1 + idx2 * 4}', key = f'{idx+1 + idx2 * 4})
+        col.text_input(f'조 추첨 대상{idx+1 + idx2 * 4}', key = f'{idx+1 + idx2 * 4}')
 # 13명이 소속될 조 이름을 넣을 위치
 
 
