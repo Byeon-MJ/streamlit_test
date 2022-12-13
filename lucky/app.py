b51141670b1b0425476bd7ab368c61d45b9dde75
Byeon_MJ <byunmj24@gmail.com>
2022-12-13 14:43:14 +0900

    123

--- lucky/app.py
+++ lucky/app.py
@@ -46,33 +46,39 @@ for idx, col in enumerate(columns2):
                         key = f'g{idx + 1 + idx2 * 4}') # n -> g
                         
 
-# 13명이 소속될 조 이름을 넣을 위치
-# st.write(st.session_state)
-
-# np.random.choice -> 추출 -> 이름, 목록 연결
-# 1. st.session_state - n, g가 섞여있음
-ss = pd.Series(st.session_state)
-# st.write(ss)
-# ss2 = ss[ss != '']
-ss2 = ss[ss.ne('')]
-# st.write[ss2]
-
-# string 관련된 메소드를 사용할 수 있게 함
-n_idx = ss2.index.str.contains('n')
-n_data = ss2[n_idx]
-# st.write(n_data)
-
-g_idx = ss2.index.str.contains('g')
-g_data = ss2[g_idx]
-# st.write(g_data)
-
-# n_data를 섞어줌(비복원)
-n_rd = np.random.choice(n_data, len(n_data), replace=False)
-st.write(n_rd)
-
-# 2. df 형태로 정리
-
-
 # <추첨 버튼>
+if st.button('추첨 시작!'):
+    # 13명이 소속될 조 이름을 넣을 위치
+    # st.write(st.session_state)
 
-# 13개의 짝을 지어서 표시해줄 그래픽
+    # np.random.choice -> 추출 -> 이름, 목록 연결
+    # 1. st.session_state - n, g가 섞여있음
+    ss = pd.Series(st.session_state)
+    # st.write(ss)
+    # ss2 = ss[ss != '']
+    ss2 = ss[ss.ne('')]
+    # st.write[ss2]
+
+    # string 관련된 메소드를 사용할 수 있게 함
+    n_idx = ss2.index.str.contains('n')
+    n_data = ss2[n_idx]
+    # st.write(n_data)
+
+    g_idx = ss2.index.str.contains('g')
+    g_data = ss2[g_idx]
+    # st.write(g_data)
+
+    # n_data를 섞어줌(비복원)
+    n_rd = np.random.choice(n_data, len(n_data), replace=False)
+    st.write(n_rd)
+    g_rd = np.random.choice(g_data, len(g_data), replace=False)
+    st.write(g_rd)
+
+    # 2. df 형태로 정리
+    # 13개의 짝을 지어서 표시해줄 그래픽
+    df = pd.DataFrame({
+        '추첨 대상자 이름' : n_rd,
+        '조 이름' : g_rd
+    })
+    st.snow()
+    st.write(df)
