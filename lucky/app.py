f85b0f215301613f47f3e34c0926ec2758d3a1b6
Byeon_MJ <byunmj24@gmail.com>
2022-12-13 14:35:33 +0900

    123

--- lucky/app.py
+++ lucky/app.py
@@ -55,12 +55,16 @@ ss = pd.Series(st.session_state)
 # st.write(ss)
 # ss2 = ss[ss != '']
 ss2 = ss[ss.ne('')]
-st.write[ss2]
+# st.write[ss2]
 
 # string 관련된 메소드를 사용할 수 있게 함
 n_idx = ss2.index.str.contains('n')
 n_data = ss2[n_idx]
 st.write(n_data)
+
+g_idx = ss2.index.str.contains('g')
+g_data = ss2[g_idx]
+st.write(g_data)
 
 
 # 2. df 형태로 정리
