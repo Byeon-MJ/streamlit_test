a8825e1b80f01ceb1b731428be2a129ce3b48d3c
Byeon_MJ <byunmj24@gmail.com>
2022-12-13 11:44:37 +0900

    Streamlit First Commit

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # streamlit_test
 Streamlit 연습 Repository
-
-* 쿠쉬쿠쉬의 역사
-[오리온, 페스츄리형 클커 '쿠쉬쿠쉬 출시](https://m.kmib.co.kr/view.asp?arcid=0012076698)
-
---
+++ first_webapp/app.py
@@ -0,0 +1,16 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    ```
+    # 제 첫 웹페이지(는 아니지만) 입니다.
+    ## 부족하지만 많이 사랑해주셔도 되고 안하셔도 됩니다.
+    * 1$ = 1,300원 이지만 좀 내렸으면 좋겠네요
+    ```
+)
+
+# https://pixabay.com/ko
+st.image(
+    'https://cdn.pixabay.com/photo/2015/07/30/22/38/pixabay-868437_960_720.jpg'
+)
