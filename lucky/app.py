954300484a462106df037f2f6d9db1ffd876c1d4
Byeon_MJ <byunmj24@gmail.com>
2022-12-13 14:36:48 +0900

    random

--- lucky/app.py
+++ lucky/app.py
@@ -60,12 +60,15 @@ ss2 = ss[ss.ne('')]
 # string 관련된 메소드를 사용할 수 있게 함
 n_idx = ss2.index.str.contains('n')
 n_data = ss2[n_idx]
-st.write(n_data)
+# st.write(n_data)
 
 g_idx = ss2.index.str.contains('g')
 g_data = ss2[g_idx]
-st.write(g_data)
+# st.write(g_data)
 
+# n_data를 섞어줌(비복원)
+n_rd = np.random.choice(n_data, len(n_data), replace=False)
+st.write(n_rd)
 
 # 2. df 형태로 정리
 
